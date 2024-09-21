cb12e2d3c5e872b1b497d232b5d35cb4fd92592d
shaohanzhong <shaomenghao@hotmail.com>
2024-09-21 18:25:25 +0800

    change default password

--- src/CRM.Domain/CRMConsts.cs
+++ src/CRM.Domain/CRMConsts.cs
@@ -1,11 +1,9 @@
-﻿using Volo.Abp.Identity;
-
-namespace CRM;
+﻿namespace CRM;
 
 public static class CRMConsts
 {
     public const string DbTablePrefix = "App";
     public const string? DbSchema = null;
-    public const string AdminEmailDefaultValue = IdentityDataSeedContributor.AdminEmailDefaultValue;
-    public const string AdminPasswordDefaultValue = IdentityDataSeedContributor.AdminPasswordDefaultValue;
+    public const string AdminEmailDefaultValue = "admin@crm.com";
+    public const string AdminPasswordDefaultValue = "2024@09$14&Start";
 }
