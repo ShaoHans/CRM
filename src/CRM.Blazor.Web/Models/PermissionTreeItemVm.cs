aec2c34052ccf03698df3c18c3c6b239db990a56
shaohanzhong <shaomenghao@hotmail.com>
2024-09-28 22:20:35 +0800

    tree

---
+++ src/CRM.Blazor.Web/Models/PermissionTreeItemVm.cs
@@ -0,0 +1,19 @@
+﻿using Volo.Abp.PermissionManagement;
+
+namespace CRM.Blazor.Web.Models;
+
+public class PermissionTreeItemVm
+{
+    public int Depth { get; set; }
+
+    public PermissionGrantInfoDto Permission { get; set; } = default!;
+
+    public List<PermissionTreeItemVm> Children { get; set; } = [];
+
+    public PermissionTreeItemVm(int depth, PermissionGrantInfoDto permission, List<PermissionTreeItemVm> children)
+    {
+        Depth = depth;
+        Permission = permission;
+        Children = children;
+    }
+}
