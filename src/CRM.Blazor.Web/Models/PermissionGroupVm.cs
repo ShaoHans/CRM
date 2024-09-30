d68b524740e4e5a551f8517ef108b84bcd351267
shaohanzhong <shaomenghao@hotmail.com>
2024-09-30 21:50:12 +0800

    update

---
+++ src/CRM.Blazor.Web/Models/PermissionGroupVm.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.PermissionManagement;
+
+namespace CRM.Blazor.Web.Models;
+
+public class PermissionGroupVm : PermissionGroupDto
+{
+    public bool GrantAll
+    {
+        get { return Permissions.All(x => x.IsGranted); }
+        set { }
+    }
+}
