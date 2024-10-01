4d33632bd90a878ddcbc10dfc70cfd704ff358cb
shaohanzhong <shaomenghao@hotmail.com>
2024-10-01 11:26:04 +0800

    refactor menu service

--- src/CRM.Blazor.Web/Services/MenuService.cs
+++ src/CRM.Blazor.Web/Services/MenuService.cs
@@ -4,8 +4,8 @@ namespace CRM.Blazor.Web.Services;
 
 public class MenuService
 {
-    Menu[] allMenus =
-    [        
+    readonly Menu[] allMenus =
+    [
         new Menu
         {
             Name = "身份认证管理",
@@ -14,15 +14,15 @@ public class MenuService
             [
                 new Menu
                 {
-                    Name = "用户",
-                    Path = "/user/list",
-                    Icon = "person"
-                },
-                 new Menu
-                {
                     Name = "角色",
                     Path = "/role/list",
                     Icon = "safety_check"
+                },
+                new Menu
+                {
+                    Name = "用户",
+                    Path = "/user/list",
+                    Icon = "person"
                 },
             ]
         }
@@ -47,7 +47,7 @@ public class MenuService
         bool deepFilter(Menu example) => filter(example) || example.Children?.Any(filter) == true;
 
         return Menus
-            .Where(category => category.Children?.Any(deepFilter) == true || filter(category))
+            .Where(category => category.Children.Any(deepFilter) == true || filter(category))
             .Select(category => new Menu
             {
                 Name = category.Name,
@@ -55,7 +55,7 @@ public class MenuService
                 Icon = category.Icon,
                 Expanded = true,
                 Children = category
-                    .Children?.Where(deepFilter)
+                    .Children.Where(deepFilter)
                     .Select(example => new Menu
                     {
                         Name = example.Name,
@@ -67,35 +67,5 @@ public class MenuService
                     .ToArray()
             })
             .ToList();
-    }
-
-    public Menu FindCurrent(Uri uri)
-    {
-        IEnumerable<Menu> Flatten(IEnumerable<Menu> e)
-        {
-            return e.SelectMany(c => c.Children != null ? Flatten(c.Children) : new[] { c });
-        }
-
-        return Flatten(Menus)
-            .FirstOrDefault(example =>
-                example.Path == uri.AbsolutePath || $"/{example.Path}" == uri.AbsolutePath
-            );
-    }
-
-    public string TitleFor(Menu example)
-    {
-        if (example != null && example.Name != "Overview")
-        {
-            return example.Title
-                ?? $"Blazor {example.Name} Component | Free UI Components by Radzen";
-        }
-
-        return "Free Blazor Components | 90+ UI controls by Radzen";
-    }
-
-    public string DescriptionFor(Menu example)
-    {
-        return example?.Description
-            ?? "The Radzen Blazor component library provides more than 90 UI controls for building rich ASP.NET Core web applications.";
     }
 }
